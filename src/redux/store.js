d2c1f4992d27d2cdf864d28c92e6ef190b4590f8
vennis <hoanganhgo28062001@gmail.com>
2022-06-17 23:30:46 +0700

    create homepage and list product

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,11 @@
-import { createStore, applyMiddleware } from "redux";
-import logger from "redux-logger";
-import thunk from "redux-thunk";
-import rootReducer from "./rootReducer";
+import { createStore, applyMiddleware } from 'redux';
+import logger from 'redux-logger';
+import thunk from 'redux-thunk';
+import rootReducer from './rootReducer';
 
 const middleware = [thunk];
 
-if (process.env.NODE_ENV === "development") {
+if (process.env.NODE_ENV === 'development') {
   middleware.push(logger);
 }
 
--- src/util/axiosClient.js
+++ src/util/axiosClient.js
@@ -1,5 +1,5 @@
-import axios from "axios";
-import queryString from "query-string";
+import axios from 'axios';
+import queryString from 'query-string';
 // Set up default config for http requests here
 
 // Please have a look at here `https://github.com/axios/axios#request-
@@ -7,12 +7,12 @@ import queryString from "query-string";
 const axiosClient = axios.create({
   baseURL: process.env.REACT_APP_API_URL,
   headers: {
-    "content-type": "application/json"
+    'content-type': 'application/json',
   },
-  paramsSerializer: (params) => queryString.stringify(params)
+  paramsSerializer: (params) => queryString.stringify(params),
 });
 axiosClient.interceptors.request.use(async (config) => {
-  const access_token = await localStorage.getItem("__token");
+  const access_token = await localStorage.getItem('__token');
   if (access_token) {
     config.headers.Authorization = `Bearer ${access_token}`;
   }
