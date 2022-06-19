a607a8648c57c498a4c4a6fa522215f351cc1df0
Loc <nguyenhoangloc143@gmail.com>
2022-06-19 09:29:56 +0700

    create cart vs orderlist

--- src/App.js
+++ src/App.js
@@ -8,6 +8,9 @@ import Login from 'pages/home/login';
 import ProductList from 'pages/store/product-list';
 import ProfilePage from 'pages/store/profile';
 import { Button } from 'antd';
+import OrderList from 'pages/store/order-list';
+import Cart from 'pages/store/cart';
+import InformationOrder from 'pages/store/information-order';
 
 const AppWrapper = () => {
   return (
@@ -26,6 +29,9 @@ const App = () => {
           <Route path="/" element={<HomePage />} />
           <Route path="/login" element={<Login />} />
           <Route path="/product-list" element={<ProductList />} />
+          <Route path="/order-list" element={<OrderList />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/information-order" element={<InformationOrder />} />
           {/* </Route> */}
           {/* <Route path="/profile" element={<ProfilePage />} /> */}
         </Routes>
